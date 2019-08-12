c44e81be13e7d4c99ff18f91028a4f7db440775a
Nick1st <54022594+Nick1st@users.noreply.github.com>
2019-08-12 17:40:40 +0200

    Added: Colored Wood completed!

--- src/main/java/nick1st/feedback_mod/Specification.java
+++ src/main/java/nick1st/feedback_mod/Specification.java
@@ -4,6 +4,6 @@ public class Specification {
 
     public static final String MODID = "feedbackmod";
     public static final String NAME = "Feedback Mod";
-    public static final String VERSION = "1.0.0a";
+    public static final String VERSION = "1.0.0b";
 	
 }
