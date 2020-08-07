0620782e8114810377e77b0a8c2163b4689c187f
Nick1st <54022594+Nick1st@users.noreply.github.com>
2020-08-07 12:23:37 +0200

    Delete Specification.java

--- src/main/java/nick1st/feedback_mod/Specification.java
+++
@@ -1,9 +0,0 @@
-package nick1st.feedback_mod;
-
-public class Specification {
-
-    public static final String MODID = "feedbackmod";
-    public static final String NAME = "Feedback Mod";
-    public static final String VERSION = "1.0.0b";
-	
-}
